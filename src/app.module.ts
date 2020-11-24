3c56e696a45eacd79cc5e144dd88c78ec5697367
dan.vieira <danemmanuelturibio@gmail.com>
2020-11-24 16:00:37 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/products-nest', {
-      useNewUrlParser: true
+    MongooseModule.forRoot('mongodb://danvieira:dan2335132@ds018258.mlab.com:18258/dnodestr', {
+      useNewUrlParser: true,
     }),
-    ProductModule
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
